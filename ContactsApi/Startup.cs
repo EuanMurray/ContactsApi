1095aa8c09ab6de3a58dd6963729f49fa0b0f672
euanm2514 <euanm2514@gmail.com>
2018-02-20 11:10:26 +0000

    Finished tutorial

--- ContactsApi/Startup.cs
+++ ContactsApi/Startup.cs
@@ -8,21 +8,23 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ContactsApi.Models;
 
 namespace ContactsApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<con>(opt => opt.UseInMemoryDatabase("TodoList"));
             services.AddMvc();
         }
 
